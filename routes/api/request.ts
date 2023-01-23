d232d18189292ae81115adc526637fc52886d7a1
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2023-01-23 10:03:53 +0100

    .

--- routes/api/request.ts
+++ routes/api/request.ts
@@ -1,6 +1,6 @@
 import { HandlerContext } from "$fresh/server.ts";
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
-import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+import * as google from "https://googleapis.deno.dev/v1/sheets:v4.ts";
+import { getJson } from "../../shared/file.ts";
 
 type DbRequest = {
   firstName: string;
@@ -14,22 +14,24 @@ export const handler = async (_req: Request, _ctx: HandlerContext) => {
   try {
     const newRequest = (await _req.json()) as DbRequest;
 
-    const client = new SmtpClient();
-    const { SEND_EMAIL, PASSWORD, RECV_EMAIL } = Deno.env.toObject();
-    const connectConfig = {
-      hostname: "smtp.gmail.com",
-      port: 465,
-      username: SEND_EMAIL,
-      password: PASSWORD,
-    };
-    await client.connectTLS(connectConfig);
-    await client.send({
-      from: SEND_EMAIL,
-      to: RECV_EMAIL,
-      subject: "Новая заявка с сайта alekcey0211-tai-master.deno.dev",
-      content: Object.values(newRequest).join("<br />"),
-    });
-    await client.close();
+    const googleKeys = await getJson("tai-ms-db-610c215eb4cf.json");
+    const spreadSheetId = "1DSI7mx-L5pEVEejqPyY2Ui8QPl-J6uoIQQQtSaec2u4";
+    const auth = new google.GoogleAuth();
+    const client = auth.fromJSON(googleKeys);
+    const sheets = new google.Sheets(client);
+    await sheets.spreadsheetsValuesAppend(
+      "Заявки!A:F",
+      spreadSheetId,
+      {
+        values: [
+          [
+            ...Object.values(newRequest),
+            new Date(Date.now()).toLocaleDateString(),
+          ],
+        ],
+      },
+      { valueInputOption: "USER_ENTERED" }
+    );
 
     return new Response(null, {
       status: 200,
@@ -38,7 +40,7 @@ export const handler = async (_req: Request, _ctx: HandlerContext) => {
       },
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return new Response(null, {
       status: 500,
       statusText: "Something went wrong",
